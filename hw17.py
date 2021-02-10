c94ba5817cefe391515f4101f4d2a02bd27841ea
Kelad_Kaal <migulaevat@gmail.com>
2021-02-10 18:22:58 +0300

    hw17

--- hw17.py
+++ hw17.py
@@ -1,10 +1,5 @@
 from GPSPhoto import gpsphoto
 import pickle
-# Get the data from image file and return a dictionary
-#data = gpsphoto.getGPSData('C:\Users\User\Downloads\Telegram Desktop')
-#rawData = gpsphoto.getRawData('C:\Users\User\Downloads\Telegram Desktop')
-
-#for tag in rawData.keys():
 print (gpsphoto.getGPSData('C:/Users/User/20210210_175742.jpg'))
 a = gpsphoto.getGPSData('C:/Users/User/20210210_175742.jpg')
 s = str(a.get('Latitude'))+","+str(a.get('Longitude'))
