7194c22a2198b504c8b77480957946768fc50503
Kelad_Kaal <migulaevat@gmail.com>
2021-02-12 13:06:46 +0300

    18.1

--- hw18.py
+++ hw18.py
@@ -1,14 +1,17 @@
 import os
 from PIL import Image
 
-img = Image.open('C:/Users/User/20210210_175742.jpg')
+print ("Write your photo address like: C:/Users/User/20210210_175742.jpg")
+
+s = input()
+img = Image.open(s)
 width,height = 800,600
 if img.size[0] < img.size[1]:
     width,height = height,width
 
 resized_img = img.resize((width, height), Image.ANTIALIAS)
 resized_img.save('photo-resized.jpg')
-a = os.path.getsize("C:/Users/User/20210210_175742.jpg")
+a = os.path.getsize(s)
 print('size before ',a)
 b = os.path.getsize("photo-resized.jpg")
 print('size after', b)
