6795d1fefc18e46d74db85f4fc07f8868c93a4f7
Kelad_Kaal <migulaevat@gmail.com>
2021-02-12 12:59:46 +0300

    12.1

--- hw12.py
+++ hw12.py
@@ -1,4 +1,13 @@
 def fib(n):
-    if n in (1, 2):
-        return 1
-    return fib(n - 1) + fib(n - 2)
+    cur = 1
+    old = 1
+    i = 1
+    while (i < n):
+        cur, old, i = cur+old, cur, i+1
+    return cur
+
+
+print ("how many numbers do you want to get?")
+n = int(input())
+for i in range(n):
+    print(fib(i))
