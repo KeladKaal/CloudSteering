6f9ba24de60047074a9d6092fb4aa5eb074a91fe
Kelad_Kaal <migulaevat@gmail.com>
2021-03-08 14:59:06 +0300

    hw-dop.1

--- hw_dop.py
+++ hw_dop.py
@@ -1,7 +1,16 @@
-def n_arr(widths):
+def n_arr(widths, step=None):
+    """
+    Создается n-мерный список в следующем порядке - список из n пустых строк, где n это первый элемент из width
+    """
+    if step == None:
+
+        step = len(widths)
     if len(widths) == 1:
         return ['' for _ in range(widths[0])]
-    return [n_arr(widths[1:])] * widths[1]
+    return [n_arr(widths[len(widths) - step + 1:], step - 1) for _ in range(widths[len(widths) - step])]
 
 
-print(n_arr([2, 2, 3]))
+x = n_arr([2, 2, 3])
+print(x)
+x[0][1][2] = 3
+print(x)
